acd5e0e46688b9db47af266ad6515f10f39e1f41
AV <netnoc@gmail.com>
2017-04-21 02:19:20 +0200

    Changes in how we start workers

--- start-worker.sh
+++ start-worker.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-celery worker -A runner --prefetch-multiplier 1 -c 1 \
+celery worker -A runner --prefetch-multiplier 1 -c 1 -q \
   --without-gossip --without-mingle --without-heartbeat \
-  --loglevel=info \
-  # -q
+  # --loglevel=info
